19bbf2c1330dfb5468d8c84dedbc9e9ed0c736b2
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-08-07 16:44:53 -0400

    Started services section, adjusted back to top button, added photos to meet the owners. Time: 11hrs 15mins

--- script.js
+++ script.js
@@ -48,21 +48,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-let mybutton = document.getElementById("myBtn");
+// Back to top code
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+$('document').ready(() => {
+    window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+    function scrollFunction() {
+        if (document.body.scrollTop > 56 || document.documentElement.scrollTop > 56) {
+            $('#backToTop').fadeIn(200)
+        } else {
+            $('#backToTop').fadeOut(200)
+        }
+    }
+})
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
