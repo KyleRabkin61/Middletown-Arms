14b0ee670ad1be5bbf64e8aa01fbd25a4154273f
KyleRabkin61 <kylerabkin6161@gmail.com>
2025-08-11 22:28:38 -0400

    Continued working on responsivness of site, added products to product section, continued styling products page, started contact page, started styling contact page. Time: 22hrs 30mins

--- script.js
+++ script.js
@@ -114,3 +114,59 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// contact js
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
+
+window.onload = function () {
+    document.getElementsByClassName('input-container').textContent = ''
+};
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = document.getElementById("contactForm");
+    const formData = new FormData(form);
+
+    for (let [name, value] of formData.entries()) {
+        if (value.trim() === "") {
+            alert("Please fill out all fields in order for your message to be sent.");
+            return;
+        }
+    }
+
+    fetch(form.action, {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                alert("Message sent!");
+                form.reset();
+            } else {
+                alert("There was a problem sending your message.");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("There was a problem sending your message.");
+        });
+}
